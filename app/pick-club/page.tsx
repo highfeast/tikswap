531db6e98a86d9041c1145961cadbb7d82c2fdaf
kelvinpraises <kelvinpraises@gmail.com>
2024-03-16 20:38:10 +0100

    chore: revert back to button lol

--- app/pick-club/page.tsx
+++ app/pick-club/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
 
 const page = () => {
@@ -7,9 +7,7 @@ const page = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-16 p-0 flex-1 overflow-scroll">
       <div className="flex flex-col justify-center items-center gap-2 p-0">
-        <p className="font-atyp text-[64px] text-[#02071E]">
-          Enter your fan zone
-        </p>
+        <p className="font-atyp text-[64px] text-[#02071E]">Pick your club</p>
         <p className="font-medium text-sm leading-[17px] text-[#484E62]">
           TikSwap empowers fans to reclaim the ticketing process. Click the
           power button to discover how.
