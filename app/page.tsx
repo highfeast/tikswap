393ce5fed744cd15457515462705c9ca4114aea3
kelvinpraises <kelvinpraises@gmail.com>
2024-03-16 20:15:29 +0100

    feat: init app

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-16 p-0 flex-1 overflow-scroll">
+      <div className="flex flex-col justify-center items-center gap-2 p-0">
+        <p className="font-atyp text-[64px] text-[#02071E]">Enter your fan zone</p>
+        <p className="font-medium text-sm leading-[17px] text-[#484E62]">
+          TikSwap empowers fans to reclaim the ticketing process. Click the
+          power button to discover how.
+        </p>
+      </div>
+      <button className="w-[136px] h-[136px] flex bg-transparent items-center justify-center rounded-full border-[6px] border-solid border-[#34C759]" onClick={}>
+        <img src="/Standby.svg" alt="standby image" />
+      </button>
+    </div>
+  );
+};
+
+export default page;
