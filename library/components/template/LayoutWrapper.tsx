6e437ffc386f5e951b83b023d6ae420ac593b1f0
kelvinpraises <kelvinpraises@gmail.com>
2024-03-16 20:11:58 +0100

    feat: add atyp, outfit, inter font

---
+++ library/components/template/LayoutWrapper.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { Inter, Outfit } from "next/font/google";
+import localFont from "next/font/local";
+
+import RootProvider from "@/library/providers/provider";
+import { cn } from "@/library/utils";
+import WrapAround from "./WrapAround";
+
+const inter = Inter({ subsets: ["latin"], preload: true });
+const outfit = Outfit({
+  subsets: ["latin"],
+  preload: true,
+  variable: "--font-outfit",
+});
+
+const atyp = localFont({
+  src: [
+    {
+      path: "../../../public/AtypDisplay-Semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../../public/AtypDisplay-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../../public/AtypDisplay-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-atyp",
+  preload: true,
+});
+
+const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className={cn(inter.className, outfit.variable, atyp.variable)}>
+        <RootProvider>
+          <WrapAround>{children}</WrapAround>
+        </RootProvider>
+      </body>
+    </html>
+  );
+};
+
+export default LayoutWrapper;
